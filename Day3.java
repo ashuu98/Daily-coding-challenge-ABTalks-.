697d2ab2c8eaf67b396930a902ac99e698a05faf
Ashutosh Kumar <ashutosh123@gmail.com>
2026-03-06 09:27:23 +0530

    Day3

---
+++ Day3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Day3{
+    public static boolean isPalindrome(int num){
+        int temp=num;
+        int n=0;
+        while(temp>0){
+            int digit=temp%10;
+            n=n*10+digit;
+            temp/=10;
+        }
+        return n ==num;
+    }
+    public static void main(String[] args){
+     Scanner sc= new Scanner(System.in);
+     int n=sc.nextInt();
+     if(isPalindrome(n))
+        System.out.print("Number is Palindrome");
+    else
+        System.out.print("Number is not Palindrome");
+    }
+}
