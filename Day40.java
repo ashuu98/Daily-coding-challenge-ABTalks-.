f3c85a50a86eb05e8249fcd33c6de0c1443cb345
Ashutosh Kumar <ashutoshkr260@gmail.com>
2026-04-17 09:24:33 +0530

    Valid BST

---
+++ Day40.java
@@ -0,0 +1,49 @@
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Day40 {
+
+    // Insert into BST
+    public static TreeNode insert(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+
+        if (val < root.val) {
+            root.left = insert(root.left, val);
+        } else if (val > root.val) {
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    // Valid bst
+    public static boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private static boolean validate(TreeNode node, long min, long max) {
+        if (node == null) return true;
+
+        if (node.val <= min || node.val >= max) return false;
+
+        return validate(node.left, min, node.val) &&
+               validate(node.right, node.val, max);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = null;
+
+        int[] values = {5, 3, 7, 2, 4};
+        for (int val : values) {
+            root = insert(root, val);
+        }
+
+        System.out.println("Is valid BST: " + isValidBST(root));
+    }
+}
